202fb096b843449546c9e9c3fa6e96abdb121229
Omar G.R <Omega_skull@MacBook-Pro-de-Omar.local>
2019-08-02 21:29:50 -0500

    Initial commit

---
+++ demo/src/main/java/com/example/demo/FileActions.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.example.demo;
+
+import java.io.StringReader;
+import java.util.Map;
+
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+
+import com.couchbase.client.java.search.SearchQuery;
+import com.couchbase.client.java.search.queries.DisjunctionQuery;
+import com.couchbase.client.java.search.queries.MatchQuery;
+import com.couchbase.client.java.search.result.SearchQueryResult;
+
+
+
+/**
+ * @author Omega_skull
+ *
+ */
+public class FileActions {
+
+	/**
+	 * 
+	 */
+	public FileActions() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void fileAdd() {
+		
+	}
+	public void fuzzySearch(){
+		
+		String word = "Book";
+		MatchQuery titleFuzzy = SearchQuery.match(word).fuzziness(1).field("title");
+		MatchQuery titleSimple = SearchQuery.match(word).field("title");
+		DisjunctionQuery ftsQuery = SearchQuery.disjuncts(titleSimple, titleFuzzy);
+		 
+		/*
+		 * SearchQueryResult result =
+		 * movieRepository.getCouchbaseOperations().getCouchbaseBucket().query( new
+		 * SearchQuery(indexName, ftsQuery).highlight().limit(20));
+		 * printResults(result);
+		 */
+		
+	}
+	
+	public void list() {
+		
+	
+		
+	}
+	
+
+}
